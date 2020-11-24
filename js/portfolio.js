781547319e39a86b752ae16a04c428edcdc2f9f4
ashleynewman <47230237+ashleynewman@users.noreply.github.com>
2020-11-24 12:23:28 -0500

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,195 @@
+document.querySelector('#Dystopia').addEventListener("click", function() {
+  if (document.getElementById("d-movies").style.display == 'block') {
+    document.getElementById("d-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("d-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#gv').addEventListener("click", function() {
+  if (document.getElementById("gv-movies").style.display == 'block') {
+    document.getElementById("gv-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("gv-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#ad').addEventListener("click", function() {
+  if (document.getElementById("ad-movies").style.display == 'block') {
+    document.getElementById("ad-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("ad-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#sup').addEventListener("click", function() {
+  if (document.getElementById("sup-movies").style.display == 'block') {
+    document.getElementById("sup-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("sup-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#vac').addEventListener("click", function() {
+  if (document.getElementById("vac-movies").style.display == 'block') {
+    document.getElementById("vac-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("vac-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#lol').addEventListener("click", function() {
+  if (document.getElementById("lol-movies").style.display == 'block') {
+    document.getElementById("lol-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("lol-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#dark').addEventListener("click", function() {
+  if (document.getElementById("dark-movies").style.display == 'block') {
+    document.getElementById("dark-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("dark-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#wack').addEventListener("click", function() {
+  if (document.getElementById("wack-movies").style.display == 'block') {
+    document.getElementById("wack-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("wack-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#hol').addEventListener("click", function() {
+  if (document.getElementById("hol-movies").style.display == 'block') {
+    document.getElementById("hol-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("hol-movies").style.display = 'block';
+  }
+});
+document.querySelector('#romcom').addEventListener("click", function() {
+  if (document.getElementById("romcom-movies").style.display == 'block') {
+    document.getElementById("romcom-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("romcom-movies").style.display = 'block';
+  }
+});
+document.querySelector('#crime').addEventListener("click", function() {
+  if (document.getElementById("crime-movies").style.display == 'block') {
+    document.getElementById("crime-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("crime-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#sci').addEventListener("click", function() {
+  if (document.getElementById("sci-movies").style.display == 'block') {
+    document.getElementById("sci-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("sci-movies").style.display = 'block';
+  }
+});
+document.querySelector('#sing').addEventListener("click", function() {
+  if (document.getElementById("sing-movies").style.display == 'block') {
+    document.getElementById("sing-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("sing-movies").style.display = 'block';
+  }
+});
+document.querySelector('#dance').addEventListener("click", function() {
+  if (document.getElementById("dance-movies").style.display == 'block') {
+    document.getElementById("dance-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("dance-movies").style.display = 'block';
+  }
+});
+document.querySelector('#doll').addEventListener("click", function() {
+  if (document.getElementById("doll-movies").style.display == 'block') {
+    document.getElementById("doll-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("doll-movies").style.display = 'block';
+  }
+});
+document.querySelector('#sk').addEventListener("click", function() {
+  if (document.getElementById("sk-movies").style.display == 'block') {
+    document.getElementById("sk-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("sk-movies").style.display = 'block';
+  }
+});
+document.querySelector('#scifi').addEventListener("click", function() {
+  if (document.getElementById("scifi-movies").style.display == 'block') {
+    document.getElementById("scifi-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("scifi-movies").style.display = 'block';
+  }
+});
+document.querySelector('#mon').addEventListener("click", function() {
+  if (document.getElementById("mon-movies").style.display == 'block') {
+    document.getElementById("mon-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("mon-movies").style.display = 'block';
+  }
+});
+document.querySelector('#slash').addEventListener("click", function() {
+  if (document.getElementById("slash-movies").style.display == 'block') {
+    document.getElementById("slash-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("slash-movies").style.display = 'block';
+  }
+});
+document.querySelector('#hs').addEventListener("click", function() {
+  if (document.getElementById("hs-movies").style.display == 'block') {
+    document.getElementById("hs-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("hs-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#anim').addEventListener("click", function() {
+  if (document.getElementById("anim-movies").style.display == 'block') {
+    document.getElementById("anim-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("anim-movies").style.display = 'block';
+  }
+});
+document.querySelector('#child').addEventListener("click", function() {
+  if (document.getElementById("child-movies").style.display == 'block') {
+    document.getElementById("child-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("child-movies").style.display = 'block';
+  }
+});
+
+document.querySelector('#fam').addEventListener("click", function() {
+  if (document.getElementById("fam-movies").style.display == 'block') {
+    document.getElementById("fam-movies").style.display = 'none';
+  }
+  else {
+    document.getElementById("fam-movies").style.display = 'block';
+  }
+});
